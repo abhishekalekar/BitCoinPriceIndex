6c960859527d2ee282ca38fc0a610b03f80674df
abhishekalekar <abhishekalekar@gmail.com>
2020-04-05 00:37:59 +1300

    Updates

---
+++ precache-manifest.1391fbd57d09cb7cb32b22b242584fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6b663b7ab6a1d0fed985cf61e8ca67",
+    "url": "/BitCoinPriceIndex/index.html"
+  },
+  {
+    "revision": "0cd5e9a713cda6fd579f",
+    "url": "/BitCoinPriceIndex/static/css/main.8e01d342.chunk.css"
+  },
+  {
+    "revision": "667a6488bbe4dad8cb7a",
+    "url": "/BitCoinPriceIndex/static/js/2.7770b8bd.chunk.js"
+  },
+  {
+    "revision": "745374e0907cbc9ae847f64b8efd9f9d",
+    "url": "/BitCoinPriceIndex/static/js/2.7770b8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd5e9a713cda6fd579f",
+    "url": "/BitCoinPriceIndex/static/js/main.3abf4b3b.chunk.js"
+  },
+  {
+    "revision": "174192f056474ab6b058",
+    "url": "/BitCoinPriceIndex/static/js/runtime-main.f74e3408.js"
+  }
+]);
